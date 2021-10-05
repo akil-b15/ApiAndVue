c3c949ab6df60715a60c493708dfff11d7be99d3
Akil Ahmed <akilahmed.b15@gmail.com>
2021-10-05 10:31:58 +0600

    api route, controller and data fetch

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Resources\Article as ArticleResource;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get article
+        $article = Article::paginate(15);
+
+        // return collection of article as a resource 
+        return ArticleResource::collection($article);
+    }  
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// list articles
+Route::get('/articles', [ArticleController::class, 'index']);
+
+// list single article 
+Route::get('/article/{id}', [ArticleController::class, 'show']);
+
+// create new article 
+Route::post('/article', [ArticleController::class, 'store']);
+
+// update article 
+Route::put('/article', [ArticleController::class, 'store']);
+
+// delete article 
+Route::delete('/article', [ArticleController::class, 'destroy']);
